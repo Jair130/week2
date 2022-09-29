64bcc096f57bfb67b92ed665581f7cfafa1e1498
Jair130 <100359718+Jair130@users.noreply.github.com>
2022-09-29 16:40:39 +0100

    Update Week2.java

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -16,9 +16,9 @@ public class Week2 {
     public static void main(String[] args) {
         // TODO code application logic here
 <<<<<<< HEAD
+        
         System.out.println ("Hello Class!");
-=======
-        //System.out.println ("Hello git");
+
 >>>>>>> 2nd-branch
         System.out.println ("Hello 2nd Branch!");
         System.out.println ("Hello 2nd Branch!");
